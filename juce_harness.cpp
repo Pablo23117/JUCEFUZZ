1cff5ae6aac829eec2b5229d71e9d843a1dcc726
root <root@PabloIkerHP.>
2025-04-16 08:22:00 -0600

    added afl-build to gitignore

--- juce_harness.cpp
+++ juce_harness.cpp
@@ -3,35 +3,33 @@
 #include <string>
 #include <stdio.h>
 
+#include <juce_core/juce_core.h>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+
 // Entry point for AFL++ fuzzing
 int main(int argc, char *argv[]) 
 {
-    // FILE* f = fopen(argv[1], "r");
+    FILE* f = fopen(argv[1], "r");
 
-    // // Determine file size
-    // fseek(f, 0, SEEK_END);
-    // size_t size = ftell(f);
+    // Determine file size
+    fseek(f, 0, SEEK_END);
+    size_t size = ftell(f);
 
-    // char* where = new char[size];
+    char* where = new char[size];
 
-    // rewind(f);
-    // fread(where, sizeof(char), size, f);
-
-    std::ifstream file(argv[1]);
-    std::string str; 
-    while (std::getline(file, str))
-    {
-        // Process str
-    }
+    rewind(f);
+    fread(where, sizeof(char), size, f);
 
     // Convert fuzzer input to a JUCE String
-    juce::String inputString(str);
+    juce::String inputString = juce::String::fromUTF8(where);
 
     // Perform operations on the String
     juce::String upperCaseString = inputString.toUpperCase();
 
-    juce::File juceFile(inputString);
-    // delete[] where;
+    juce::File file(where);
+    delete[] where;
 
     return 0; // Indicate successful execution
 }
